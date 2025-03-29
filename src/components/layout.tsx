d65b5d04eecaf51dd072fff46fcd57f51706f19e
A Chaithanya Kumar <100756558+Chaithanya9727@users.noreply.github.com>
2025-03-29 23:20:40 +0530

    Add files via upload

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,15 +11,15 @@ export function Layout({ children }: PropsWithChildren) {
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-4">
         <div className="container mx-auto px-4 text-center text-gray-200 flex items-center justify-center gap-3">
           <img
-            src="/Sampath.jpg"
+            src="/Profile.jpg"
             alt="Sampath"
             className="h-8 w-8 rounded-full border-2 border-gray-600 shadow-md"
           />
 
           <p className="text-sm font-medium">
-            Made by Sampath |{" "}
+            Made by A Chaithanya Kumar |{" "}
             <a
-              href="https://github.com/sampath33"
+              href="https://github.com/Chaithanya9727"
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-400 hover:underline"
